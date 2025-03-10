051fb0db706d18c7759bd9a6d585f0244876c732
yoojin <euvb918@gmail.com>
2025-03-10 17:21:40 +0900

    250310

--- _중복된문자제거/sol.py
+++ _중복된문자제거/sol.py
@@ -1,8 +1,11 @@
 def solution(my_string):
     answer = ''
-    
+    for i in range(len(my_string)):
+        for j in range(1, len(my_string)):
+            if my_string[i] == my_string[j]:
+                answer += my_string[i]
 
-    return answer
+    return my_string
 
 print(solution("people")) # "peol"
 print(solution("We are the world")) # "We arthwold"
