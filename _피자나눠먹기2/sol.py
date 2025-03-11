ec750e8aaba0b63164b4ef5273f26cce025a9ebc
yoojin <euvb918@gmail.com>
2025-03-11 09:42:25 +0900

    250311

--- _피자나눠먹기2/sol.py
+++ _피자나눠먹기2/sol.py
@@ -1,8 +1,9 @@
 def solution(n):
-    for k in range(1, 601):
-        if n * k / 6 >= 1:
-            return k
-
+    for a in range(1, 101):
+        for k in range(1, 601):
+            if (k * 6 / n) == (int(a) == a):
+                print(l)
+                return k * 6 / n
 
 print(solution(6)) # 1
 print(solution(10)) # 5
