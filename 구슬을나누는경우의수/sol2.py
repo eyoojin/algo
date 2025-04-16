bbf7b07158aa487e6639333a534f8cf27f6d464c
yoojin <euvb918@gmail.com>
2025-04-16 09:41:12 +0900

    250416 ball

---
+++ 구슬을나누는경우의수/sol2.py
@@ -0,0 +1,14 @@
+def fact(n):
+    answer = 1
+    for i in range(1, n+1):
+        answer *= i
+    return answer
+
+def solution(balls, share):
+    return fact(balls) / (fact(share) * fact(balls-share))
+
+
+# 런타임에러..
+
+print(solution(3, 2)) # 3
+print(solution(5, 3)) # 10
