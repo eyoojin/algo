22026be32ebc9476687ef4aea8438182e184b944
yoojin <euvb918@gmail.com>
2025-04-18 14:21:01 +0900

    250418

--- _소인수분해/sol.py
+++ _소인수분해/sol.py
@@ -1,13 +1,13 @@
 def solution(n):
     answer = []
-    result = []
-    for i in range(2, n+1):
-        a, b = divmod(n, i)
-        if b == 0:
-            answer.append(i)
 
+    for i in range(2, n+1):
+        if n % i == 0:
+            answer.append(i)
+            n = n//i
     return answer
 
 print(solution(12)) # [2, 3]
 print(solution(17)) # [17]
 print(solution(420)) # [2, 3, 5, 7]
+print(solution(10000))
